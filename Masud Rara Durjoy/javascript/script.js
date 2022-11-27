e8c44a22e88532557762a5664342cf02832aea0a
MasudRana951 <118680879+MasudRana951@users.noreply.github.com>
2022-11-27 23:12:20 +0600

    html complete folder
    added all the necessary folders to start an HTML project

---
+++ Masud Rara Durjoy/javascript/script.js
@@ -0,0 +1,22 @@
+// $(function(){
+//     feather.replace();
+//     $('.popular_slider').slick({
+//         slidesToShow:3,
+//         arrows:false,
+//         centerMode: true,
+//         centerPadding: "200px",
+//         focusOnSelect: true,
+//       });
+// })
+
+
+// $(function(){
+//     feather.replace();
+//     $('.popular_slider').slick({
+//         slidesToShow:3,
+//         arrows:false,
+//         centerMode: true,
+//         centerPadding: "200px",
+//         focusOnSelect: true,
+//       });
+// })
